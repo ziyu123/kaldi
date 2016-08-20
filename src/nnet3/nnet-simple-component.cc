28e9953b2bdd13f3c9d76acdb6b7d3e862280259
Zhiyuan Tang <tangzy@cslt.riit.tsinghua.edu.cn>
2016-08-20 14:55:54 +0800

    PlusOneNaturalGradientAffineComponent

--- src/nnet3/nnet-simple-component.cc
+++ src/nnet3/nnet-simple-component.cc
@@ -2812,6 +2812,342 @@ void NaturalGradientAffineComponent::Add(BaseFloat alpha, const Component &other
   bias_params_.AddVec(alpha, other->bias_params_);
 }
 
+PlusOneNaturalGradientAffineComponent::PlusOneNaturalGradientAffineComponent():
+    max_change_per_sample_(0.0),
+    update_count_(0.0), active_scaling_count_(0.0),
+    max_change_scale_stats_(0.0) { }
+
+// virtual
+void PlusOneNaturalGradientAffineComponent::Resize(
+    int32 input_dim, int32 output_dim) {
+  KALDI_ASSERT(input_dim > 1 && output_dim > 1);
+  if (rank_in_ >= input_dim) rank_in_ = input_dim - 1;
+  if (rank_out_ >= output_dim) rank_out_ = output_dim - 1;
+  bias_params_.Resize(output_dim);
+  linear_params_.Resize(output_dim, input_dim);
+  OnlineNaturalGradient temp;
+  preconditioner_in_ = temp;
+  preconditioner_out_ = temp;
+  SetNaturalGradientConfigs();
+}
+
+
+void PlusOneNaturalGradientAffineComponent::Read(std::istream &is, bool binary) {
+  ReadUpdatableCommon(is, binary);  // Read the opening tag and learning rate
+  ExpectToken(is, binary, "<LinearParams>");
+  linear_params_.Read(is, binary);
+  ExpectToken(is, binary, "<BiasParams>");
+  bias_params_.Read(is, binary);
+  ExpectToken(is, binary, "<RankIn>");
+  ReadBasicType(is, binary, &rank_in_);
+  ExpectToken(is, binary, "<RankOut>");
+  ReadBasicType(is, binary, &rank_out_);
+  ExpectToken(is, binary, "<UpdatePeriod>");
+  ReadBasicType(is, binary, &update_period_);
+  ExpectToken(is, binary, "<NumSamplesHistory>");
+  ReadBasicType(is, binary, &num_samples_history_);
+  ExpectToken(is, binary, "<Alpha>");
+  ReadBasicType(is, binary, &alpha_);
+  ExpectToken(is, binary, "<MaxChangePerSample>");
+  ReadBasicType(is, binary, &max_change_per_sample_);
+  ExpectToken(is, binary, "<IsGradient>");
+  ReadBasicType(is, binary, &is_gradient_);
+  std::string token;
+  ReadToken(is, binary, &token);
+  if (token == "<UpdateCount>") {
+    ReadBasicType(is, binary, &update_count_);
+    ExpectToken(is, binary, "<ActiveScalingCount>");
+    ReadBasicType(is, binary, &active_scaling_count_);
+    ExpectToken(is, binary, "<MaxChangeScaleStats>");
+    ReadBasicType(is, binary, &max_change_scale_stats_);
+    ReadToken(is, binary, &token);
+  }
+  if (token != "<PlusOneNaturalGradientAffineComponent>" &&
+      token != "</PlusOneNaturalGradientAffineComponent>")
+    KALDI_ERR << "Expected <PlusOneNaturalGradientAffineComponent> or "
+              << "</PlusOneNaturalGradientAffineComponent>, got " << token;
+  SetNaturalGradientConfigs();
+}
+
+void PlusOneNaturalGradientAffineComponent::InitFromConfig(ConfigLine *cfl) {
+  bool ok = true;
+  std::string matrix_filename;
+  BaseFloat num_samples_history = 2000.0, alpha = 4.0,
+      max_change_per_sample = 0.0;
+  int32 input_dim = -1, output_dim = -1, rank_in = 20, rank_out = 80,
+      update_period = 4;
+  InitLearningRatesFromConfig(cfl);
+  cfl->GetValue("num-samples-history", &num_samples_history);
+  cfl->GetValue("alpha", &alpha);
+  cfl->GetValue("max-change-per-sample", &max_change_per_sample);
+  cfl->GetValue("rank-in", &rank_in);
+  cfl->GetValue("rank-out", &rank_out);
+  cfl->GetValue("update-period", &update_period);
+
+  if (cfl->GetValue("matrix", &matrix_filename)) {
+    Init(rank_in, rank_out, update_period,
+         num_samples_history, alpha, max_change_per_sample,
+         matrix_filename);
+    if (cfl->GetValue("input-dim", &input_dim))
+      KALDI_ASSERT(input_dim == InputDim() &&
+                   "input-dim mismatch vs. matrix.");
+    if (cfl->GetValue("output-dim", &output_dim))
+      KALDI_ASSERT(output_dim == OutputDim() &&
+                   "output-dim mismatch vs. matrix.");
+  } else {
+    ok = ok && cfl->GetValue("input-dim", &input_dim);
+    ok = ok && cfl->GetValue("output-dim", &output_dim);
+    if (!ok)
+      KALDI_ERR << "Bad initializer " << cfl->WholeLine();
+    BaseFloat param_stddev = 1.0 / std::sqrt(input_dim),
+        bias_stddev = 1.0, bias_mean = 0.0;
+    cfl->GetValue("param-stddev", &param_stddev);
+    cfl->GetValue("bias-stddev", &bias_stddev);
+    cfl->GetValue("bias-mean", &bias_mean);
+    Init(input_dim, output_dim, param_stddev,
+         bias_stddev, bias_mean, rank_in, rank_out, update_period,
+         num_samples_history, alpha, max_change_per_sample);
+  }
+  if (cfl->HasUnusedValues())
+    KALDI_ERR << "Could not process these elements in initializer: "
+              << cfl->UnusedValues();
+  if (!ok)
+    KALDI_ERR << "Bad initializer " << cfl->WholeLine();
+}
+
+void PlusOneNaturalGradientAffineComponent::SetNaturalGradientConfigs() {
+  preconditioner_in_.SetRank(rank_in_);
+  preconditioner_in_.SetNumSamplesHistory(num_samples_history_);
+  preconditioner_in_.SetAlpha(alpha_);
+  preconditioner_in_.SetUpdatePeriod(update_period_);
+  preconditioner_out_.SetRank(rank_out_);
+  preconditioner_out_.SetNumSamplesHistory(num_samples_history_);
+  preconditioner_out_.SetAlpha(alpha_);
+  preconditioner_out_.SetUpdatePeriod(update_period_);
+}
+
+void PlusOneNaturalGradientAffineComponent::Init(
+    int32 rank_in, int32 rank_out,
+    int32 update_period, BaseFloat num_samples_history, BaseFloat alpha,
+    BaseFloat max_change_per_sample,
+    std::string matrix_filename) {
+  rank_in_ = rank_in;
+  rank_out_ = rank_out;
+  update_period_ = update_period;
+  num_samples_history_ = num_samples_history;
+  alpha_ = alpha;
+  SetNaturalGradientConfigs();
+  KALDI_ASSERT(max_change_per_sample >= 0.0);
+  max_change_per_sample_ = max_change_per_sample;
+  CuMatrix<BaseFloat> mat;
+  ReadKaldiObject(matrix_filename, &mat); // will abort on failure.
+  KALDI_ASSERT(mat.NumCols() >= 2);
+  int32 input_dim = mat.NumCols() - 1, output_dim = mat.NumRows();
+  linear_params_.Resize(output_dim, input_dim);
+  bias_params_.Resize(output_dim);
+  linear_params_.CopyFromMat(mat.Range(0, output_dim, 0, input_dim));
+  bias_params_.CopyColFromMat(mat, input_dim);
+  is_gradient_ = false;  // not configurable; there's no reason you'd want this
+  update_count_ = 0.0;
+  active_scaling_count_ = 0.0;
+  max_change_scale_stats_ = 0.0;
+}
+
+void PlusOneNaturalGradientAffineComponent::Init(
+    int32 input_dim, int32 output_dim,
+    BaseFloat param_stddev, BaseFloat bias_stddev, BaseFloat bias_mean,
+    int32 rank_in, int32 rank_out, int32 update_period,
+    BaseFloat num_samples_history, BaseFloat alpha,
+    BaseFloat max_change_per_sample) {
+  linear_params_.Resize(output_dim, input_dim);
+  bias_params_.Resize(output_dim);
+  KALDI_ASSERT(output_dim > 0 && input_dim > 0 && param_stddev >= 0.0 &&
+               bias_stddev >= 0.0);
+  linear_params_.SetRandn(); // sets to random normally distributed noise.
+  linear_params_.Scale(param_stddev);
+  bias_params_.SetRandn();
+  bias_params_.Scale(bias_stddev);
+  bias_params_.Add(bias_mean);
+  rank_in_ = rank_in;
+  rank_out_ = rank_out;
+  update_period_ = update_period;
+  num_samples_history_ = num_samples_history;
+  alpha_ = alpha;
+  SetNaturalGradientConfigs();
+  if (max_change_per_sample > 0.0)
+    KALDI_WARN << "You are setting a positive max_change_per_sample for "
+               << "PlusOneNaturalGradientAffineComponent. But the per-component "
+               << "gradient clipping mechansim has been removed. Instead it's currently "
+               << "done at the whole model level.";
+  max_change_per_sample_ = max_change_per_sample;
+  is_gradient_ = false;  // not configurable; there's no reason you'd want this
+  update_count_ = 0.0;
+  active_scaling_count_ = 0.0;
+  max_change_scale_stats_ = 0.0;
+}
+
+void PlusOneNaturalGradientAffineComponent::Write(std::ostream &os,
+                                           bool binary) const {
+  WriteUpdatableCommon(os, binary);  // Write the opening tag and learning rate
+  WriteToken(os, binary, "<LinearParams>");
+  linear_params_.Write(os, binary);
+  WriteToken(os, binary, "<BiasParams>");
+  bias_params_.Write(os, binary);
+  WriteToken(os, binary, "<RankIn>");
+  WriteBasicType(os, binary, rank_in_);
+  WriteToken(os, binary, "<RankOut>");
+  WriteBasicType(os, binary, rank_out_);
+  WriteToken(os, binary, "<UpdatePeriod>");
+  WriteBasicType(os, binary, update_period_);
+  WriteToken(os, binary, "<NumSamplesHistory>");
+  WriteBasicType(os, binary, num_samples_history_);
+  WriteToken(os, binary, "<Alpha>");
+  WriteBasicType(os, binary, alpha_);
+  WriteToken(os, binary, "<MaxChangePerSample>");
+  WriteBasicType(os, binary, max_change_per_sample_);
+  WriteToken(os, binary, "<IsGradient>");
+  WriteBasicType(os, binary, is_gradient_);
+  WriteToken(os, binary, "<UpdateCount>");
+  WriteBasicType(os, binary, update_count_);
+  WriteToken(os, binary, "<ActiveScalingCount>");
+  WriteBasicType(os, binary, active_scaling_count_);
+  WriteToken(os, binary, "<MaxChangeScaleStats>");
+  WriteBasicType(os, binary, max_change_scale_stats_);
+  WriteToken(os, binary, "</PlusOneNaturalGradientAffineComponent>");
+}
+
+std::string PlusOneNaturalGradientAffineComponent::Info() const {
+  std::ostringstream stream;
+  stream << UpdatableComponent::Info();
+  PrintParameterStats(stream, "linear-params", linear_params_);
+  PrintParameterStats(stream, "bias", bias_params_, true);
+  stream << ", rank-in=" << rank_in_
+         << ", rank-out=" << rank_out_
+         << ", num_samples_history=" << num_samples_history_
+         << ", update_period=" << update_period_
+         << ", alpha=" << alpha_
+         << ", max-change-per-sample=" << max_change_per_sample_;
+  if (update_count_ > 0.0 && max_change_per_sample_ > 0.0) {
+    stream << ", avg-scaling-factor=" << max_change_scale_stats_ / update_count_
+           << ", active-scaling-portion="
+           << active_scaling_count_ / update_count_;
+  }
+  return stream.str();
+}
+
+void PlusOneNaturalGradientAffineComponent::Propagate(const ComponentPrecomputedIndexes *indexes,
+                                const CuMatrixBase<BaseFloat> &in,
+                                 CuMatrixBase<BaseFloat> *out) const {
+
+  // No need for asserts as they'll happen within the matrix operations.
+  out->CopyRowsFromVec(bias_params_); // copies bias_params_ to each row
+  // of *out.
+  CuMatrix<BaseFloat> linear_params_temp(linear_params_);
+  linear_params_temp.Add(1.0);
+  out->AddMatMat(1.0, in, kNoTrans, linear_params_temp, kTrans, 1.0);
+}
+
+
+Component* PlusOneNaturalGradientAffineComponent::Copy() const {
+  return new PlusOneNaturalGradientAffineComponent(*this);
+}
+
+PlusOneNaturalGradientAffineComponent::PlusOneNaturalGradientAffineComponent(
+    const PlusOneNaturalGradientAffineComponent &other):
+    AffineComponent(other),
+    rank_in_(other.rank_in_),
+    rank_out_(other.rank_out_),
+    update_period_(other.update_period_),
+    num_samples_history_(other.num_samples_history_),
+    alpha_(other.alpha_),
+    preconditioner_in_(other.preconditioner_in_),
+    preconditioner_out_(other.preconditioner_out_),
+    max_change_per_sample_(other.max_change_per_sample_),
+    update_count_(other.update_count_),
+    active_scaling_count_(other.active_scaling_count_),
+    max_change_scale_stats_(other.max_change_scale_stats_) {
+  SetNaturalGradientConfigs();
+}
+
+void PlusOneNaturalGradientAffineComponent::Update(
+    const std::string &debug_info,
+    const CuMatrixBase<BaseFloat> &in_value,
+    const CuMatrixBase<BaseFloat> &out_deriv) {
+  CuMatrix<BaseFloat> in_value_temp;
+
+  in_value_temp.Resize(in_value.NumRows(),
+                       in_value.NumCols() + 1, kUndefined);
+  in_value_temp.Range(0, in_value.NumRows(),
+                      0, in_value.NumCols()).CopyFromMat(in_value);
+
+  // Add the 1.0 at the end of each row "in_value_temp"
+  in_value_temp.Range(0, in_value.NumRows(),
+                      in_value.NumCols(), 1).Set(1.0);
+
+  CuMatrix<BaseFloat> out_deriv_temp(out_deriv);
+
+  CuMatrix<BaseFloat> row_products(2,
+                                   in_value.NumRows());
+  CuSubVector<BaseFloat> in_row_products(row_products, 0),
+      out_row_products(row_products, 1);
+
+  // These "scale" values get will get multiplied into the learning rate (faster
+  // than having the matrices scaled inside the preconditioning code).
+  BaseFloat in_scale, out_scale;
+
+  preconditioner_in_.PreconditionDirections(&in_value_temp, &in_row_products,
+                                            &in_scale);
+  preconditioner_out_.PreconditionDirections(&out_deriv_temp, &out_row_products,
+                                             &out_scale);
+
+  // "scale" is a scaling factor coming from the PreconditionDirections calls
+  // (it's faster to have them output a scaling factor than to have them scale
+  // their outputs).
+  BaseFloat scale = in_scale * out_scale;
+
+  CuSubMatrix<BaseFloat> in_value_precon_part(in_value_temp,
+                                              0, in_value_temp.NumRows(),
+                                              0, in_value_temp.NumCols() - 1);
+  // this "precon_ones" is what happens to the vector of 1's representing
+  // offsets, after multiplication by the preconditioner.
+  CuVector<BaseFloat> precon_ones(in_value_temp.NumRows());
+
+  precon_ones.CopyColFromMat(in_value_temp, in_value_temp.NumCols() - 1);
+
+  BaseFloat local_lrate = scale * learning_rate_;
+  update_count_ += 1.0;
+  bias_params_.AddMatVec(local_lrate, out_deriv_temp, kTrans,
+                         precon_ones, 1.0);
+  linear_params_.AddMatMat(local_lrate, out_deriv_temp, kTrans,
+                           in_value_precon_part, kNoTrans, 1.0);
+}
+
+void PlusOneNaturalGradientAffineComponent::ZeroStats()  {
+  update_count_ = 0.0;
+  max_change_scale_stats_ = 0.0;
+  active_scaling_count_ = 0.0;
+}
+
+void PlusOneNaturalGradientAffineComponent::Scale(BaseFloat scale) {
+  update_count_ *= scale;
+  max_change_scale_stats_ *= scale;
+  active_scaling_count_ *= scale;
+  linear_params_.Scale(scale);
+  bias_params_.Scale(scale);
+}
+
+void PlusOneNaturalGradientAffineComponent::Add(BaseFloat alpha, const Component &other_in) {
+  const PlusOneNaturalGradientAffineComponent *other =
+      dynamic_cast<const PlusOneNaturalGradientAffineComponent*>(&other_in);
+  KALDI_ASSERT(other != NULL);
+  update_count_ += alpha * other->update_count_;
+  max_change_scale_stats_ += alpha * other->max_change_scale_stats_;
+  active_scaling_count_ += alpha * other->active_scaling_count_;
+  linear_params_.AddMat(alpha, other->linear_params_);
+  bias_params_.AddVec(alpha, other->bias_params_);
+}
+
+
 std::string FixedAffineComponent::Info() const {
   std::ostringstream stream;
   stream << Component::Info();
--- src/nnet3/nnet-simple-component.h
+++ src/nnet3/nnet-simple-component.h
@@ -724,6 +724,96 @@ class NaturalGradientAffineComponent: public AffineComponent {
       const CuMatrixBase<BaseFloat> &out_deriv);
 };
 
+/// Keywords: natural gradient descent, NG-SGD, naturalgradient.  For
+/// the top-level of the natural gradient code look here, and also in
+/// nnet-precondition-online.h.
+/// NaturalGradientAffineComponent is
+/// a version of AffineComponent that has a non-(multiple of unit) learning-rate
+/// matrix.  See nnet-precondition-online.h for a description of the technique.
+/// It is described, under the name Online NG-SGD, in the paper "Parallel
+/// training of DNNs with Natural Gradient and Parameter Averaging" (ICLR
+/// workshop, 2015) by Daniel Povey, Xiaohui Zhang and Sanjeev Khudanpur.
+/// Linear_params plus one before using, we get PlusOneNaturalGradientAffineComponent.
+class PlusOneNaturalGradientAffineComponent: public AffineComponent {
+ public:
+  virtual std::string Type() const { return "PlusOneNaturalGradientAffineComponent"; }
+  virtual void Read(std::istream &is, bool binary);
+  virtual void Write(std::ostream &os, bool binary) const;
+  void Init(int32 input_dim, int32 output_dim,
+            BaseFloat param_stddev, BaseFloat bias_stddev, BaseFloat bias_mean,
+            int32 rank_in, int32 rank_out, int32 update_period,
+            BaseFloat num_samples_history, BaseFloat alpha,
+            BaseFloat max_change_per_sample);
+  void Init(int32 rank_in, int32 rank_out, int32 update_period,
+            BaseFloat num_samples_history,
+            BaseFloat alpha, BaseFloat max_change_per_sample,
+            std::string matrix_filename);
+  // this constructor does not really initialize, use Init() or Read().
+  PlusOneNaturalGradientAffineComponent();
+  virtual void Resize(int32 input_dim, int32 output_dim);
+  virtual void InitFromConfig(ConfigLine *cfl);
+  virtual std::string Info() const;
+  virtual Component* Copy() const;
+  virtual void Scale(BaseFloat scale);
+  virtual void Add(BaseFloat alpha, const Component &other);
+  virtual void Propagate(const ComponentPrecomputedIndexes *indexes,
+                         const CuMatrixBase<BaseFloat> &in,
+                         CuMatrixBase<BaseFloat> *out) const;
+  // copy constructor
+  explicit PlusOneNaturalGradientAffineComponent(
+      const PlusOneNaturalGradientAffineComponent &other);
+  virtual void ZeroStats();
+
+ private:
+  // disallow assignment operator.
+  PlusOneNaturalGradientAffineComponent &operator= (
+      const PlusOneNaturalGradientAffineComponent&);
+
+  // Configs for preconditioner.  The input side tends to be better conditioned ->
+  // smaller rank needed, so make them separately configurable.
+  int32 rank_in_;
+  int32 rank_out_;
+  int32 update_period_;
+  BaseFloat num_samples_history_;
+  BaseFloat alpha_;
+
+  OnlineNaturalGradient preconditioner_in_;
+
+  OnlineNaturalGradient preconditioner_out_;
+
+  // If > 0, max_change_per_sample_ is the maximum amount of parameter
+  // change (in L2 norm) that we allow per sample, averaged over the minibatch.
+  // This was introduced in order to control instability.
+  // Instead of the exact L2 parameter change, for
+  // efficiency purposes we limit a bound on the exact
+  // change.  The limit is applied via a constant <= 1.0
+  // for each minibatch, A suitable value might be, for
+  // example, 10 or so; larger if there are more
+  // parameters.
+  BaseFloat max_change_per_sample_;
+
+  // update_count_ records how many updates we have done.
+  double update_count_;
+
+  // active_scaling_count_ records how many updates we have done,
+  // where the scaling factor is active (not 1.0).
+  double active_scaling_count_;
+
+  // max_change_scale_stats_ records the sum of scaling factors
+  // in each update, so we can compute the averaged scaling factor
+  // in Info().
+  double max_change_scale_stats_;
+
+  // Sets the configs rank, alpha and eta in the preconditioner objects,
+  // from the class variables.
+  void SetNaturalGradientConfigs();
+
+  virtual void Update(
+      const std::string &debug_info,
+      const CuMatrixBase<BaseFloat> &in_value,
+      const CuMatrixBase<BaseFloat> &out_deriv);
+};
+
 
 /// FixedAffineComponent is an affine transform that is supplied
 /// at network initialization time and is not trainable.
