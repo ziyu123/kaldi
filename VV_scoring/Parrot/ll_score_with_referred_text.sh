86431659f04524e65025950a39bc4e6e3caadbe1
tzyll <tangzy@cslt.riit.tsinghua.edu.cn>
2017-08-30 22:37:13 +0800

    score based on average likelihood with referred text

---
+++ VV_scoring/Parrot/ll_score_with_referred_text.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. ./path.sh
+
+data_dir=data
+lang_dir=data/lang
+nnet3_dir=model
+avg_likelihood_ref=5.57615
+
+compute-fbank-feats --config=$nnet3_dir/conf/fbank.conf scp:$data_dir/wav.scp ark:$data_dir/feats.ark
+
+compile-train-graphs --read-disambig-syms=$lang_dir/phones/disambig.int \
+  $nnet3_dir/tree \
+  $nnet3_dir/final.mdl \
+  $lang_dir/L.fst \
+  "ark:utils/sym2int.pl --map-oov 64 -f 2- $lang_dir/words.txt $data_dir/text|" ark:- | \
+  ll-score-with-referred-text --referred-average-likelihood=$avg_likelihood_ref \
+    --transition-scale=1.0 --acoustic-scale=0.1 --self-loop-scale=0.1 --frames-per-chunk=50 --extra-left-context=0 --extra-right-context=0 --extra-left-context-initial=-1 --extra-right-context-final=-1 --use-gpu=no --beam=10 --retry-beam=40 $nnet3_dir/final.mdl ark:- ark:$data_dir/feats.ark ark:tmp.ark
+
+rm $data_dir/feats.ark tmp.ark
